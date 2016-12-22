8f35ee44fe8929c479fc6bffdea97ea965b3a0a8
Anish Balaji <balaji.anish8@gmail.com>
2016-12-21 21:44:39 -0800

    testing rename

--- js/init.js
+++ js/init.js
@@ -6,3 +6,9 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// $("#about-link").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#about-container").offset().top
+//     }, 800);
+// });
