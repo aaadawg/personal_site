18c9b77121a517d61df762626fd541448831228f
Anish Balaji <balaji.anish8@gmail.com>
2016-12-23 00:34:47 -0800

    navbar links temporary

--- js/init.js
+++ js/init.js
@@ -7,8 +7,53 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-// $("#about-link").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#about-container").offset().top
-//     }, 800);
-// });
+
+// TEMP SCROLL FOR NAVBAR LINKS
+$("#about-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#index-banner").offset().top
+    }, 800);
+});
+
+$("#project-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#project-banner").offset().top
+    }, 800);
+});
+
+$("#consulting-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#consulting-banner").offset().top
+    }, 800);
+});
+
+$("#contact-link").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#collab-banner").offset().top
+    }, 800);
+});
+
+$("#about-link-side").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#index-banner").offset().top
+    }, 800);
+});
+
+$("#project-link-side").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#project-banner").offset().top
+    }, 800);
+});
+
+$("#consulting-link-side").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#consulting-banner").offset().top
+    }, 800);
+});
+
+$("#contact-link-side").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#collab-banner").offset().top
+    }, 800);
+});
+
